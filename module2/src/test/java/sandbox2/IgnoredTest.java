9905b4e53833fe49075081a2bb4a49d858918fae
Nadezhda Burnasheva <nadezhda.burnasheva@gmail.com>
2015-12-14 10:33:43 +0300

    remove test from ignore

--- module2/src/test/java/sandbox2/IgnoredTest.java
+++ module2/src/test/java/sandbox2/IgnoredTest.java
@@ -8,7 +8,7 @@ import org.testng.annotations.Test;
  */
 public class IgnoredTest {
 
-    @Test (enabled = false)
+    @Test(retryAnalyzer = MyRetryAnalyzer.class)
     public void testShouldBeIgnored(){
         assert true;
     }
